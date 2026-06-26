24f076a69b499be4f33486ad83bb60ccbe9044c4
ArthurFedj <arthur.feger@gmail.com>
2026-06-26 10:30:37 +0200

    Ajout des commandes ajouter et liste

--- todo.py
+++ todo.py
@@ -1,8 +1,14 @@
 import sys
 
 def main():
-    print("mes tâches:")
+    commande = sys.argv[1]
+    if commande == "ajouter":
+        with open("taches.txt", "a") as f:
+            f.write(sys.argv[2] + "\n")
+    elif commande == "liste":
+        with open("taches.txt", "r") as f:
+            print(f.read())
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
 
